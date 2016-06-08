ae75e9791f76d2be749d743b4cc7a6b72d4e65b5
Nathaniel Giron <nathaniel.giron@gmail.com>
2016-06-08 00:55:20 -0700

    make app production ready

--- index.js
+++ index.js
@@ -25,7 +25,11 @@ app.use(stylus.middleware(
 ));
 app.use(express.static(__dirname + '/public'));
 
-mongoose.connect('mongodb://localhost/mainlycodes');
+if(env === 'development') {
+	mongoose.connect('mongodb://localhost/mainlycodes');
+} else {
+	mongoose.connect('mongodb://ngiron:zzgbabq@ds011024.mlab.com:11024/mainlycodes');
+}
 var db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error...'));
 db.once('open', function callback() {
@@ -48,6 +52,6 @@ app.get('*', function(req, res) {
 	});
 });
 
-var port = 3030;
+var port = process.env.PORT || 3030;
 app.listen(port);
 console.log('Listening on port ' + port + '...');
