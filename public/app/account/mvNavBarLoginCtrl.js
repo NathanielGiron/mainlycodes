7a29287faf004bc79f5ee142cec0813189dcbb36
Nathaniel Giron <nathaniel.giron@gmail.com>
2016-06-08 23:36:18 -0700

    implemented secure passwords, added signout functionality

--- public/app/account/mvNavBarLoginCtrl.js
+++ public/app/account/mvNavBarLoginCtrl.js
@@ -1,4 +1,4 @@
-angular.module('app').controller('mvNavBarLoginCtrl', function($scope, $http, mvIdentity, mvNotifier, mvAuth) {
+angular.module('app').controller('mvNavBarLoginCtrl', function($scope, $http, mvIdentity, mvNotifier, mvAuth, $location) {
 	$scope.identity = mvIdentity;
 	$scope.signin = function(username, password) {
 		mvAuth.authenticateUser(username, password).then(function(success) {
@@ -7,6 +7,15 @@ angular.module('app').controller('mvNavBarLoginCtrl', function($scope, $http, mv
 			} else {
 				mvNotifier.notify('Username/Password combination incorrect.', 'error');
 			}
+		});
+	}
+
+	$scope.signout = function() {
+		mvAuth.logoutUser().then(function() {
+			$scope.username = "";
+			$scope.password = "";
+			mvNotifier.notify('You have successfully signed out!', 'success');
+			$location.path('/');
 		});
 	}
 });
--- server/config/mongoose.js
+++ server/config/mongoose.js
@@ -1,4 +1,5 @@
-var mongoose = require('mongoose');
+var mongoose = require('mongoose'),
+	crypto = require('crypto');
 
 module.exports = function(config) {
 	mongoose.connect(config.db);
@@ -11,15 +12,39 @@ module.exports = function(config) {
 	var userSchema = mongoose.Schema({
 		firstName: String,
 		lastName: String,
-		username: String
+		username: String,
+		salt: String,
+		hashed_pwd: String
 	});
+	userSchema.methods = {
+		authenticate: function(passwordToMatch) {
+			return hashPwd(this.salt, passwordToMatch) === this.hashed_pwd;
+		}
+	}
 
 	var User = mongoose.model('User', userSchema);
 
 	User.find({}).exec(function(err, collection) {
 		if(collection.length === 0) {
-			User.create({firstName:'Nat', lastName:'Giron', username:'nat'});
-			User.create({firstName:'Monte', lastName:'Hoover', username:'monte'});
+			var salt, hash;
+			salt = createSalt();
+			hash = hashPwd(salt, 'nat');
+			User.create({firstName:'Nat', lastName:'Giron', username:'nat', salt: salt, hashed_pwd: hash});
+			salt = createSalt();
+			hash = hashPwd(salt, 'monte');
+			User.create({firstName:'Monte', lastName:'Hoover', username:'monte', salt: salt, hashed_pwd: hash});
 		}
 	});
+}
+
+function createSalt() {
+	return crypto.randomBytes(128).toString('base64');
+}
+
+function hashPwd(salt, pwd) {
+	var hmac = crypto.createHmac('sha1', salt);
+	hmac.setEncoding('hex');
+	hmac.write(pwd);
+	hmac.end();
+	return hmac.read();
 }
