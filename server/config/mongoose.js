978f2b323db8071001bc192392486b5bd45313c0
Nathaniel Giron <nathaniel.giron@gmail.com>
2016-06-10 00:12:09 -0700

    User Profile, Updating Data

--- server/config/mongoose.js
+++ server/config/mongoose.js
@@ -1,5 +1,5 @@
 var mongoose = require('mongoose'),
-	encrypt = require('../utilities/encryption');
+	userModel = require('../models/User');
 
 module.exports = function(config) {
 	mongoose.connect(config.db);
@@ -9,32 +9,7 @@ module.exports = function(config) {
 		console.log('mainlycodes db opened');
 	});
 
-	var userSchema = mongoose.Schema({
-		firstName: String,
-		lastName: String,
-		username: String,
-		salt: String,
-		hashed_pwd: String,
-		roles: [String]
-	});
-	userSchema.methods = {
-		authenticate: function(passwordToMatch) {
-			return encrypt.hashPwd(this.salt, passwordToMatch) === this.hashed_pwd;
-		}
-	}
+	userModel.createDefaultUsers();
 
-	var User = mongoose.model('User', userSchema);
-
-	User.find({}).exec(function(err, collection) {
-		if(collection.length === 0) {
-			var salt, hash;
-			salt = encrypt.createSalt();
-			hash = encrypt.hashPwd(salt, 'nat');
-			User.create({firstName:'Nat', lastName:'Giron', username:'nat', salt: salt, hashed_pwd: hash, roles: ['admin']});
-			salt = encrypt.createSalt();
-			hash = encrypt.hashPwd(salt, 'monte');
-			User.create({firstName:'Monte', lastName:'Hoover', username:'monte', salt: salt, hashed_pwd: hash, roles: []});
-		} 
-	});
-}
+};
 
